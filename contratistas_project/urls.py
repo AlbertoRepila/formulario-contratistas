2f529f71c7ea3c588e6b519f7a5a5f6955d7c8e8
Alberto Repila Guzman (Internship) <alberto.repilaguzman@scpec.net>
2025-06-09 11:43:51 +0200

    Mensaje explicativo de los cambios

--- contratistas_project/urls.py
+++ contratistas_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin  #importa el panel de administración django
 from django.urls import path, include #path para rutas especificas, include para usar rutas definidas en otro archivos, como los de mi app
 from django.contrib.auth import views as auth_views #importa las vistas de login y logout que trae django por defecto
 from django.shortcuts import redirect
+from . import views
 
 
 def home(request):
@@ -13,6 +14,7 @@ urlpatterns = [  #lista de urls que django utiliza para saber a donde mandarte c
     path('formularios/', include('formularios.urls')),  #lista de formularios, crear nuevo formulario, editar formulario 
     path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),  #pantalla de inicio de sesión. se activa automaticamente si no estas logueado
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'), #se te cierra la sesión y te lleva en funcion de lo que este en setings en logout_redirect
+    path('run-migrate/', views.run_migrate, name='run_migrate'), #ruta a la base de datos
     path('', home),
 
 
